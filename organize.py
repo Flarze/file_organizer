440a5f8098c4ba89fa2cc943c693764db1c7a652
Flarze <75508039+Flarze@users.noreply.github.com>
2026-05-01 18:33:56 +0200

    Add overview plan

---
+++ organize.py
@@ -0,0 +1,10 @@
+# A file organizer that sorts the downloads folder into different categories based on the files extension.
+# Images (.png .svg .jpg .jpeg .gif .AVIF and .WebP)
+# Docs (.docx .pdf .txt .odt .rtf .md .csv .json)
+# Videos (.mp4 .mov .mkv .avi .wmv .webm)
+# Music (.mp3 .wav .flac .aac .m4a)
+# Programs (.exe .jar .bin .msi .sh .py .js .bat)
+# Other (anything not listed before)
+# Duplicate files are appended with (amount + 1)
+# Creates destination folder if it doesn't exist yet. 
+# When does prints an exit message such as "successfully sorted folder" or "failed to sort folder"
